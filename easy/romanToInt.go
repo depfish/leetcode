a9779d04b7ab8035eb14f8e1e06d4e886f7a8644
depfish <ftcd2016@gmail.com>
2019-11-05 01:08:09 +0800

    add romatoint

---
+++ easy/romanToInt.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	romanToInt("LVIII")
+
+}
+
+func romanToInt(s string)  {
+
+	//var (
+	//	I int = 1
+	//	V int = 5
+	//	X int = 10
+	//	L int = 50
+	//	C int = 100
+	//	D int = 500
+	//	M int = 1000
+	//)
+
+	fmt.Println(string(s[0]))
+
+}
