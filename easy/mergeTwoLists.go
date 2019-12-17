271f4345b4540fdfcf5506b52835a412304b4d67
depfish <ftcd2016@gmail.com>
2019-12-18 06:00:32 +0800

    update

---
+++ easy/mergeTwoLists.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) {
+
+	//type ListNode struct {
+	//     Val int
+	//     Next *ListNode
+	// }
+
+}
